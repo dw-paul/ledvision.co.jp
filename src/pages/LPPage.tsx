bc4aedf9315db1e93483bedfb52fe693115d0a65
unknown <paul@daniel-works.co.jp>
2026-02-09 19:55:29 +0900

    Add pages folder with ComingSoon and LPPage

---
+++ src/pages/LPPage.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { LPHeader } from '../components/lp/LPHeader';
+import { LPHero } from '../components/lp/LPHero';
+import { LPPriceBlock } from '../components/lp/LPPriceBlock';
+import { LPStrengths } from '../components/lp/LPStrengths';
+import { LPUseCases } from '../components/lp/LPUseCases';
+import { LPFAQ } from '../components/lp/LPFAQ';
+import { LPFinalCTA } from '../components/lp/LPFinalCTA';
+import { LPContact } from '../components/lp/LPContact';
+
+export function LPPage() {
+  return (
+    <div className="relative bg-zinc-950 text-white">
+      <LPHeader />
+      <LPHero />
+      <LPPriceBlock />
+      <LPStrengths />
+      <LPUseCases />
+      <LPFAQ />
+      <LPFinalCTA />
+      <LPContact />
+    </div>
+  );
+}
