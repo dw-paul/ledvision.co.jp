8b463a0ded65e7b94508cb59a4b1730c5375ad2e
unknown <paul@daniel-works.co.jp>
2026-02-09 19:50:05 +0900

    Add React Router for / and /lp

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import { LPHeader } from './components/lp/LPHeader';
-import { LPHero } from './components/lp/LPHero';
-import { LPPriceBlock } from './components/lp/LPPriceBlock';
-import { LPStrengths } from './components/lp/LPStrengths';
-import { LPUseCases } from './components/lp/LPUseCases';
-import { LPFAQ } from './components/lp/LPFAQ';
-import { LPFinalCTA } from './components/lp/LPFinalCTA';
-import { LPContact } from './components/lp/LPContact';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ComingSoon } from './pages/ComingSoon';
+import { LPPage } from './pages/LPPage';
 
 export default function App() {
   return (
-    <div className="relative bg-zinc-950 text-white">
-      <LPHeader />
-      <LPHero />
-      <LPPriceBlock />
-      <LPStrengths />
-      <LPUseCases />
-      <LPFAQ />
-      <LPFinalCTA />
-      <LPContact />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<ComingSoon />} />
+        <Route path="/lp" element={<LPPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
